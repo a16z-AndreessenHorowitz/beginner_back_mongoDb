b062b7a36033e5277bd8cafa06b2e7f8b8111c9a
Trần Minh Huy <justhuytran@MacBook-Air-cua-Tran.local>
2025-08-24 11:00:50 +0700

    Sub-menu client theo cha và con

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,7 +1,9 @@
 const Product=require("../../models/product.model")
 const systemCongfig=require("../../config/system");
 const ProductCateGory = require("../../models/product-category.model");
-const productHelper=require("../../helpers/products")
+const productHelper=require("../../helpers/products");
+const productCategoryHelper = require("../../helpers/products-category");
+
 // [GET] /products
 module.exports.index= async (req,res)=>{
     const products = await Product.find({
@@ -49,4 +51,28 @@ module.exports.detail= async(req, res)=>{
       pageTitle:"Chi tiết sản phẩm",
       product: product,
   })
+}
+
+// [GET] /products/:slugCategory
+module.exports.category= async(req, res)=>{
+  const category =await ProductCateGory.findOne({
+    slug:req.params.slugCategory,
+    deleted:false,
+    status:"active",
+  })
+
+  const listSubCategory=await productCategoryHelper.getSubCategory(category.id)
+  const listSubCategoryId=listSubCategory.map(item=>item.id)
+
+    const products=await Product.find({
+      product_category_id:{$in:[category.id, ...listSubCategoryId]},
+      deleted:false,
+    }).sort({position:"desc"})
+
+      const newProducts=productHelper.priceNewProducts(products)
+
+  res.render("client/pages/products/index.pug",{
+        pageTitle:category.title,
+        products: newProducts
+    })
 }
---
+++ helpers/products-category.js
@@ -0,0 +1,23 @@
+const ProductCategory=require("../models/product-category.model")
+module.exports.getSubCategory=async(parentId)=>{
+  const getCategory=async(parentId)=>{
+    const subs=await ProductCategory.find({
+      parent_id:parentId,
+      status:"active",
+      deleted:false,
+    })
+    let allSub=[...subs] //lấy tất cả các phần tử củ
+     
+    for(const sub of subs){
+      const childs=await getCategory(sub.id)
+      allSub=allSub.concat(childs)
+    }
+
+  return allSub
+  }
+  
+  const result= await getCategory(parentId)
+  return result
+}
+
+//exports ra ngoài không dùng dc đệ quy
