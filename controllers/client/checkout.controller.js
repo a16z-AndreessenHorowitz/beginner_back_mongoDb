58a4118ee6df73c777366bd7390c397351689755
Trần Minh Huy <justhuytran@MacBook-Air-cua-Tran.local>
2025-08-25 10:31:53 +0700

    Trang success của order thành công

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -78,3 +78,26 @@ module.exports.order=async(req,res)=>{
   res.redirect(`/checkout/success/${order.id}`)
 }
 
+// [GET]/checkout/success/:orderId 
+module.exports.success=async(req,res)=>{
+  const order=await Order.findOne({
+    _id:req.params.orderId
+  })
+  for(const product of order.products){
+    const productInfo=await Product.findOne({
+      _id: product.product_id
+    }).select("title thumbnail")
+    //thêm key mới
+    product.productInfo=productInfo
+    //Thêm giá mới
+    product.priceNew=productHelper.priceNewProduct(product)
+    //tổng tiền
+    product.totalPrice=product.priceNew*product.quantity
+  }
+  //tổng tiền của đơn hàng
+  order.totalPrice=order.products.reduce((sum,item)=>sum+item.totalPrice,0)
+  res.render("client/pages/checkout/success",{
+    pageTitle:"Đặt hàng thành công", 
+    order:order
+  })
+}
